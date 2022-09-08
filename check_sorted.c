229d19f5499e30f24a41c83b8ed462042f2d582b
jiwoo ahn <ilp_rincipe@naver.com>
2022-09-08 17:06:12 +0900

    다시 짜기

--- check_sorted.c
+++ check_sorted.c
@@ -6,7 +6,7 @@
 /*   By: jiwahn <jiwahn@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 20:01:23 by jiwahn            #+#    #+#             */
-/*   Updated: 2022/09/07 23:00:35 by jiwahn           ###   ########.fr       */
+/*   Updated: 2022/09/08 12:21:47 by jiwahn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ int	is_sorted_sort_3(t_data data, int pos)
 	int num3 = start_node->content;
 
 	printf("%d %d %d\n", num1, num2, num3);
-	if ((pos == A_BTM && (num1 > num3 && num1 > num2)) || \
-			(pos == A_TOP && (num3 > num2 && num3 > num1)))
+	if ((pos == A_BTM && (num3 > num1 && num3 > num2)) || \
+			(pos == A_TOP && (num1 > num2 && num1 > num3)))
 	{
 		printf("case 1\n");
 		operation_rotate(data.stack[0]);
@@ -44,7 +44,7 @@ int	is_sorted_sort_3(t_data data, int pos)
 		}
 		return (-1);
 	}
-	else if (!(pos % 2) && (num1 > num2 && num1 > num3))
+	else if (!(pos % 2) && (num3 > num1 && num3 > num2))
 	{
 		printf("case 3\n");
 		if (pos == B_BTM)
